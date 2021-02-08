17fb4b68f94e3dc27d2de581ccee8594f0c96c85
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-02-08 21:10:08 +0530

    Call fizzbuzz function to Get Fizz when I pass in multiple of 3

--- tdd/final/fizzbuzz.js
+++ tdd/final/fizzbuzz.js
@@ -1,4 +1,7 @@
 function fizzbuzz(value){
+if(value % 3 === 0){
+    return "Fizz"
+}
 return value.toString()
 }
 
--- tdd/final/test.js
+++ tdd/final/test.js
@@ -3,3 +3,7 @@ const fizzbuzz = require('./fizzbuzz');
 test('Get "1", when I pass in 1', () => {
   expect(fizzbuzz(1)).toBe("1");
 });
+
+test('Get "Fizz" when I pass in multiple of 3 ', () => {
+  expect(fizzbuzz(3)).toBe("Fizz");
+});
