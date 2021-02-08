21eb8f7acf689a21d3dfe0c8b8d54af26fd78cfb
Priya Gupta <pgupta000205@gmail.com>
2021-02-09 02:58:54 +0530

    feat(tdd): should return 2 when passed 2

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,4 +1,4 @@
-function fizzbuz(){
- return("1");
+function fizzbuz(param){
+ return(param.toString())
 }
 module.exports= fizzbuz;
--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -4,3 +4,8 @@ test('should return "1" when passed 1', ()=>{
     const result= fizzbuz(1);
     expect(result).toBe('1');
 })
+
+test('should return "2" when passed 2', ()=>{
+    const result= fizzbuz(2);
+    expect(result).toBe('2');
+})
