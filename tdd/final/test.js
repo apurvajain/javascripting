49c67223b0f9dfe78089a3e7e88411b2fae0195f
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-02-08 21:19:03 +0530

    Call fizzbuzz function to Get FizzBuzz when I pass in multiple of 3 and 5

--- tdd/final/test.js
+++ tdd/final/test.js
@@ -11,3 +11,7 @@ test('Get "Fizz" when I pass in multiple of 3 ', () => {
 test('Get "Buzz" when I pass in multiple of 5', () => {
   expect(fizzbuzz(5)).toBe("Buzz");
 });
+
+test('Get FizzBuzz" when I pass in 15, a multiple of 3 and 5', () => {
+  expect(fizzbuzz(15)).toBe("FizzBuzz");
+});
