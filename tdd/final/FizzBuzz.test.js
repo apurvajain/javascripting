a76de9c455a42b7e6fdbbeec6a4e749b62e60a76
Anukriti <anukritigarg13@gmail.com>
2021-02-09 07:37:37 +0530

    refactor(tdd):Refactoring test code

--- tdd/final/FizzBuzz.test.js
+++ tdd/final/FizzBuzz.test.js
@@ -1,8 +1,5 @@
 const fizzBuzz=require('./FizzBuzzExercise');
 
-test("Callable function",()=>{
-    fizzBuzz();
-})
 test('Get "1", when pass in 1',()=>{
     const received=fizzBuzz(1);
     expect(received).toBe("1");
