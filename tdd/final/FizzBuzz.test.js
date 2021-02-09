63711bdd7cbae16e430916730b3cfeee8435316d
Anukriti <anukritigarg13@gmail.com>
2021-02-09 07:36:12 +0530

    feat(tdd):FizzBuzz returns '1' when passed 1

--- tdd/final/FizzBuzz.test.js
+++ tdd/final/FizzBuzz.test.js
@@ -3,3 +3,7 @@ const fizzBuzz=require('./FizzBuzzExercise');
 test("Callable function",()=>{
     fizzBuzz();
 })
+test('Get "1", when pass in 1',()=>{
+    const received=fizzBuzz(1);
+    expect(received).toBe("1");
+})
--- tdd/final/FizzBuzzExercise.js
+++ tdd/final/FizzBuzzExercise.js
@@ -1,4 +1,5 @@
 function fizzBuzz(){
+    return "1";
 
 }
 
