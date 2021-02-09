18efbd5c9e9cbe62b8d08156e452f7c5e2ff579d
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-09 11:51:01 +0530

    chore(jw): add jaavscript-workshop assignment files

---
+++ javascript-workshop/#1UpperCase.js
@@ -0,0 +1,7 @@
+//1
+function upperCase(input){
+   return input.toUpperCase();
+}
+console.log(upperCase('hey'));
+
+module.exports = upperCase;
---
+++ javascript-workshop/#3DoubleAll.js
@@ -0,0 +1,9 @@
+//3
+// const numbers = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9];
+function doubleAll(numbers) {
+    return numbers.map(function(value){
+        return value*2;
+    });
+}
+
+  module.exports = doubleAll
