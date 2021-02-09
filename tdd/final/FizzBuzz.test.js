4131b8ccce4b8efbfbd75a6e2212d77db5a691cd
Anukriti <anukritigarg13@gmail.com>
2021-02-09 07:39:18 +0530

    feat(tdd):FizzBuzz returns '2' when passed 2

--- tdd/final/FizzBuzz.test.js
+++ tdd/final/FizzBuzz.test.js
@@ -4,3 +4,8 @@ test('Get "1", when pass in 1',()=>{
     const received=fizzBuzz(1);
     expect(received).toBe("1");
 })
+
+test('Get "2", when pass in 2',()=>{
+    const received=fizzBuzz(2);
+    expect(received).toBe("2");
+})
--- tdd/final/FizzBuzzExercise.js
+++ tdd/final/FizzBuzzExercise.js
@@ -1,5 +1,5 @@
-function fizzBuzz(){
-    return "1";
+function fizzBuzz(value){
+    return value.toString();
 
 }
 
