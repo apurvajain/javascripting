5f9b3b46c1b7c4209d7bf2f2eef7700096a2e6e8
Anukriti <anukritigarg13@gmail.com>
2021-02-09 07:59:43 +0530

    feat(home-work):Add recursive function for hof map

---
+++ home-work/RecursionMap.js
@@ -0,0 +1,17 @@
+function recMap(array,func,newArray){
+    if(array.length===0){
+        return newArray;
+    }
+   
+    newArray.push(func(array[0]));
+    recFilter(array.slice(1),func,newArray);
+    return newArray;
+}
+
+const array=[1,2,3,4,5,6];
+
+const squaredArray=recFilter(array,function(value){
+    return value*value;
+},[])
+
+console.log(squaredArray);
