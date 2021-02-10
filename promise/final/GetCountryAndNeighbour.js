aec3215ac60b5ed174a80698464b8b56fa6d8a05
Anukriti <anukritigarg13@gmail.com>
2021-02-10 10:42:42 +0530

    feat(promise):Add function to practice async HTTP and nested callbacks

---
+++ promise/final/GetCountryAndNeighbour.js
@@ -0,0 +1,34 @@
+const https=require('https');
+
+function getCountryData(country){
+    https.get(`https://restcountries.eu/rest/v2/name/${country}`,function (response){
+        let data="";
+        response.on("data",function(chunk){
+            data+=chunk;
+        });
+        response.on("end",function(){
+            const jsonData=JSON.parse(data);
+            console.log("Data Received, Moving to other countries");
+            const [neighbour]=jsonData[0].borders;
+            console.log(neighbour);
+            if(!neighbour){
+                return;
+            }
+            https.get(`https://restcountries.eu/rest/v2/name/${neighbour}`,function(response){
+                let data="";
+                response.on("data",function(chunk){
+                    data+=chunk;
+                });
+                response.on("end",function(){
+                    const jsonDataNeighbour=JSON.parse(data);
+                    console.log(jsonDataNeighbour);
+                })
+
+            })
+        })
+    }).on("error",function(errorMessage){
+        console.log(errorMessage);
+    })
+}
+
+getCountryData('germany');
