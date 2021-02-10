cd5f3b02ea3793ef167a326424ee14a7e100861b
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-10 22:23:04 +0530

    feat(promises): add fulfill a promise exercise

---
+++ promise/exercises/fulfilling-a-promise/FulfillingAPromise.js
@@ -0,0 +1,13 @@
+var promise;
+var promise = new Promise(function (fulfill, reject) {
+  setTimeout(function () {
+    fulfill("FULFILLED!");
+  }, 300);
+});
+var fulfillAPromise = function () {
+  promise.then((res) => {
+    console.log(res);
+  });
+};
+fulfillAPromise();
+module.exports = { fulfillAPromise, promise };
