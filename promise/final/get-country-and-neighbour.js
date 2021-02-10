c7ab69c647b36bf64e2910e70d2142d429620f30
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-02-10 12:32:35 +0530

    feat:add get-country-neighbour.js

---
+++ promise/final/get-country-and-neighbour.js
@@ -0,0 +1,49 @@
+const https = require("https");
+
+function getCountryAndNeighbour(country) {
+  https
+    .get(`https://restcountries.eu/rest/v2/name/${country}`, (resp) => {
+      let data = "";
+
+      // A chunk of data has been received.
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on("end", () => {
+        const parsedData = JSON.parse(data);
+        console.log(`<--------${country} country: START-------->`);
+        console.log(parsedData);
+        console.log(`<---------${country} country: END--------->`);
+
+        // Get neighbour country 2
+        
+        const [neighbour] = parsedData[0].borders;
+        if (!neighbour) return;
+
+        https.get(
+          `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+          (resp) => {
+            let data = "";
+
+            // A chunk of data has been received.
+            resp.on("data", (chunk) => {
+              data += chunk;
+            });
+
+            resp.on("end", () => {
+              console.log(`<--------${country} neighbour: START-------->`);
+              console.log(JSON.parse(data));
+              console.log(`<---------${country} neighbour: END--------->`);
+            });
+          }
+        );
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+}
+
+getCountryAndNeighbour("portugal");
