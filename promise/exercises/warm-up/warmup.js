da5ceea4c53d819e9a50147b9fb6d090d92fc834
swetha <swetha.gumpena0607@gmail.com>
2021-02-10 17:55:27 +0530

    feat(warm-up): implement promise with timeout

---
+++ promise/exercises/warm-up/warmup.js
@@ -0,0 +1,11 @@
+// setTimeout(() => {
+//     console.log('TIMED OUT');
+//   }, 300);
+
+  function  warmup(){
+    setTimeout(() => {
+      console.log('TIMED OUT!');
+    }, 300);
+  }
+
+  module.exports = warmup
