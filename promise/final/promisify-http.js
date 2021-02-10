c1ba46c8e5f5866100eda7a5ffc0c1382cb88870
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-02-10 12:33:34 +0530

    feat:add promissify.js

---
+++ promise/final/promisify-http.js
@@ -0,0 +1,44 @@
+const https = require("https");
+
+function httpRequest(url) {
+  return new Promise(function (resolve, reject) {
+    https
+      .get(url, (resp) => {
+        let data = "";
+
+        
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        
+        resp.on("end", () => {
+          resolve(JSON.parse(data));
+        });
+      })
+      .on("error", (err) => {
+        console.log("Error: " + err.message);
+        reject(err);
+      });
+  });
+}
+
+function getCountryAndNeighbour(country) {
+  httpRequest(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then((countryData) => {
+      console.log(`<--------${country} country: START-------->`);
+      console.log(countryData);
+      console.log(`<---------${country} country: END--------->`);
+
+      const [neighbour] = countryData[0].borders;
+      if (!neighbour) return;
+      return httpRequest(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then((neighbourData) => {
+      console.log(`<--------${country} neighbour: START-------->`);
+      console.log(neighbourData);
+      console.log(`<---------${country} neighbour: END--------->`);
+    });
+}
+
+getCountryAndNeighbour("portugal");
