81ec9f47044f6d297991d7c2fc74c38aece761ab
Anukriti <anukritigarg13@gmail.com>
2021-02-10 22:52:11 +0530

    fix(shortcuts):Change toBe to toEqual for object data type

--- promise/exercises/shortcuts/Shortcuts.test.js
+++ promise/exercises/shortcuts/Shortcuts.test.js
@@ -1,14 +1,14 @@
 const {promiseResolved,promiseRejected,executePromise}=require('./Shortcuts');
 
 test("Promise1 should be resolved to 'IT IS RESOLVED!'",()=>{
-    expect(promiseResolved).resolves.toBe("IT IS RESOLVED");
+    expect(promiseResolved).resolves.toBe("IT IS RESOLVED!");
 })
 
 test("Promise2 should be resolved to error object'",()=>{
-    expect(promiseRejected).rejects.toBe(new Error('IT IS REJECTED!'));
+    expect(promiseRejected).rejects.toEqual(new Error('IT IS REJECTED!'));
 })
 
-test("Should print 'IT IS REJECTED' to the console",()=>{
+test("Should print 'IT IS REJECTED!' to the console",()=>{
     const consoleSpy = jest.spyOn(console, "log");
     executePromise();
     setTimeout(() => {
