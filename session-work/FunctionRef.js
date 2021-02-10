001a1e1b4bc8c0694ce03ef59a93a05209cac34e
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-10 07:12:38 +0530

    feat(session-work):add call by ref example

---
+++ session-work/FunctionRef.js
@@ -0,0 +1,12 @@
+function tryToChangeValues(numbers) {
+  numbers = {
+    randomKey: "anjali",
+  };
+}
+
+let numbers = {
+  num1: 100,
+  num2: 200,
+};
+tryToChangeValues(numbers);
+console.log(numbers);
