c45ea3c1f53aa6fdf718f108d4c336d02a538541
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-02-10 12:33:10 +0530

    feat:add get-country-data.js

---
+++ promise/final/get-country-data.js
@@ -0,0 +1,29 @@
+
+const https = require("https");
+
+function getCountryData(country) {
+  https
+    .get(`https://restcountries.eu/rest/v2/name/${country}`, (resp) => {
+      let data = "";
+
+      // A chunk of data has been received.
+      resp.on("data", (chunk) => {
+        
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on("end", () => {
+        console.log(`<--------${country}: START-------->`)
+        console.log(JSON.parse(data));
+        console.log(`<---------${country}: END--------->`)
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+}
+
+getCountryData("portugal");
+getCountryData("usa");
+getCountryData("germany");
