ebe913468c5b8994ff7f1de7a0f370d7cd8883b0
asmita <asmitahajra@gmail.com>
2021-02-12 03:48:45 +0530

    feat(promises): add files multiple promises

---
+++ promise/exercises/multiple-promises/MultipleProm.test.js
@@ -0,0 +1,11 @@
+const all=require('./MultipleProm');
+
+
+test("Should log ['Promise1','Promise2'] to the console",(done)=>{
+    const consoleSpy = jest.spyOn(console, "log");
+    all(getPromise1(),getPromise2()).then(console.log);
+    setTimeout(() => {
+        expect(consoleSpy).toHaveBeenCalledWith(['Promise1','Promise2']);
+        done()
+    }, 300)
+})
