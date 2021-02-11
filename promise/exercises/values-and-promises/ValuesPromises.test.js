1ad097d6d0e1e81c163cf75efef672655c1b919a
asmita <asmitahajra@gmail.com>
2021-02-12 03:33:35 +0530

    feat(promises): add files values and promises

---
+++ promise/exercises/values-and-promises/ValuesPromises.test.js
@@ -0,0 +1,20 @@
+const { promise, attachTitle, executePromise } = require('./ValuesPromises');
+
+
+test("Should prepend 'DR. ' to first argument and return result", () => {
+    const received = attachTitle('Anukriti');
+    expect(received).toBe('DR. Anukriti');
+})
+
+test("Promise should resolve with value 'MANHATTAN'", () => {
+    expect(promise).resolves.toBe('MANHATTAN');
+})
+
+test("Should print 'DR. MANHATTAN' to the console", () => {
+    const consoleSpy = jest.spyOn(console, "log");
+    executePromise();
+    setTimeout(() => {
+        expect(consoleSpy).toHaveBeenCalledWith('DR. MANHATTAN');
+
+    }, 300)
+})
