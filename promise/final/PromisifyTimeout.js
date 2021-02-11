aac20b508ae180693e8a940a43e76f1b1c64be05
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-11 10:34:49 +0530

    feat(promise): add promisify timeout

---
+++ promise/final/PromisifyTimeout.js
@@ -0,0 +1,10 @@
+const wait = function (seconds) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+};
+
+wait(1)
+  .then(() => {
+    console.log("1 second passed");
+    return wait(1);
+  })
+  .then(() => console.log("2 second passed"));
