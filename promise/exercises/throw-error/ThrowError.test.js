a0e2283a34267ef89d01cbced9998d229f81a2ce
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-11 14:59:38 +0530

    feat(ex): capture error through promise

---
+++ promise/exercises/throw-error/ThrowError.test.js
@@ -0,0 +1,20 @@
+const {
+  parsedPromised,
+  executePromise
+
+} = require('./ThrowError');
+
+
+test("Promise should reject with value 'Unexpected token u in JSON at position 0' ", () => {
+
+    expect(parsedPromised()).rejects.toEqual('Unexpected token u in JSON at position 0');
+})
+
+test("Should print 'Unexpected token u in JSON at position 0' to the console", (done) => {
+  const consoleSpy = jest.spyOn(console, "log");
+  executePromise();
+  setTimeout(() => {
+      expect(consoleSpy).toHaveBeenCalledWith('Unexpected token u in JSON at position 0');
+      done();
+  }, 300)
+})
