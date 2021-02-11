b0c168df76b4a02e6a1d4d318ffcefa166254f0b
asmita <asmitahajra@gmail.com>
2021-02-11 13:23:10 +0530

    feat(promises): add files warm up

---
+++ promise/exercises/warm-up/WarmUp.js
@@ -0,0 +1,10 @@
+
+function warmUp() {
+  setTimeout(() => {
+    console.log("TIMED OUT!");
+  }, 300);
+}
+// function warmUp(){
+
+// }
+module.exports = warmUp;
---
+++ promise/exercises/warm-up/WarmUp.test.js
@@ -0,0 +1,16 @@
+const warmUp = require("./WarmUp");
+
+// Wait for all the expects to finish before you complete your test
+// Use done for that
+
+//jest doesnt inheretently handle asynchronous behaviour, wont
+//wait for expect to run. hence need done, jest will wait till done
+test("should print TIMED OUT! after 300ms", (done) => {
+  const spy = jest.spyOn(console, "log");  
+  warmUp();
+  setTimeout(() => {
+    expect(spy).toHaveBeenCalledWith("TIMED OUT!");
+    //expect().toNotHaveBeenCalled
+    done(); 
+  }, 300);
+}); 
