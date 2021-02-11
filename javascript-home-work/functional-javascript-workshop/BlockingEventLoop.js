abe8a440b9e1d241b008c1785e92f741432c88c0
Anukriti <anukritigarg13@gmail.com>
2021-02-11 20:00:19 +0530

    feat(functional-javascript-workshop): Modify synchronus function to work asynchronus

---
+++ javascript-home-work/functional-javascript-workshop/BlockingEventLoop.js
@@ -0,0 +1,20 @@
+function repeat(operation, num) {
+    // modify this so it can be interrupted
+    if (num <= 0) return;
+    operation();
+
+    if (num % 50 === 0) {
+        setTimeout(() => {
+            return repeat(operation, --num);
+        });
+    }
+    else {
+        return repeat(operation, --num);
+    }
+
+}
+
+module.exports = repeat
+
+
+
