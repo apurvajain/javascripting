ba01408446001ebbff0d119008c8ae7a8c01eaad
asmita <asmitahajra@gmail.com>
2021-02-12 03:12:13 +0530

    feat(promises): add files fulfill a promise

---
+++ promise/exercises/fulfill-a-promise/FulfillPromise.js
@@ -0,0 +1,14 @@
+var promise = new Promise(function (fulfill, reject) {
+  setTimeout(function () {
+    fulfill("FULFILLED!");
+  }, 300);
+});
+var fulfillAPromise = function () {
+  promise.then((res) => {
+    console.log(res);
+  });
+};
+fulfillAPromise();
+module.exports = promise;
+
+  
---
+++ promise/exercises/fulfill-a-promise/FulfillPromise.test.js
@@ -0,0 +1,13 @@
+const promise = require("./FulfillPromise");
+
+test("promise should resolve with a value FULFILLED! after 300ms", () => {
+  expect(promise).resolves.toEqual("FULFILLED!");
+});
+
+test("promise resolved value FULFILLED! should be printed to console", (done) => {
+  const consoleSpy = jest.spyOn(console, "log");
+  setTimeout(() => {
+    expect(consoleSpy).toHaveBeenCalledWith("FULFILLED!");
+    done();
+  }, 300);
+});
