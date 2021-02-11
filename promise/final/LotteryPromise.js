03b6a2733d1016e715a22f9e6b59804392cc61a1
Anukriti <anukritigarg13@gmail.com>
2021-02-11 10:23:16 +0530

    update(final): Update functionality to use catch to handle rejected promise

--- promise/final/LotteryPromise.js
+++ promise/final/LotteryPromise.js
@@ -1,6 +1,11 @@
-const lotteryPromise = new Promise(function (resolve, reject) {
-    resolve("You won lottery");
+const lotteryPromise = new Promise((resolve, reject)=> {
+    if(Math.random()>0.6){
+        resolve("You won lottery :)");
+    }
+    else{
+        reject("You lost your money :(")
+    }
+    
 });
-lotteryPromise.then((message)=>{
-    console.log(message);
-})
+
+lotteryPromise.then(console.log()).catch(console.log);
