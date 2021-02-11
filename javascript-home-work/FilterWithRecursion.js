0c76d9848ce4321195626d8debeba44a76089e36
Pushya Tiwari <pushya.tiwari@gmail.com>
2021-02-11 13:26:21 +0530

    feat(Hof): add filter with recursion

--- javascript-home-work/FilterWithRecursion.js
+++ javascript-home-work/FilterWithRecursion.js
@@ -3,8 +3,9 @@ let arr = [1, 2, 3, 4, 5]
 function isEven(i) {
     return i%2 === 0
 }
+const newA = [];
 function filterWithRecur(arr, f) {
-    let newA = [];
+     
       if (arr.length === 0) return false;
       let isEven = f(arr[0]);
       newA.push(isEven)
