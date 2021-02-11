d99ac16f744193dd9c0f1947e76cb5aeefa189e4
Anukriti <anukritigarg13@gmail.com>
2021-02-11 16:23:52 +0530

    fix(reject-promise): Reject with a new Error(message) rather than passing a custom error object

--- promise/exercises/reject-promise/RejectPromise.js
+++ promise/exercises/reject-promise/RejectPromise.js
@@ -1,9 +1,9 @@
 var promise = new Promise(function (fulfill, reject) {
     setTimeout(()=>{
-        const errorObject={
-            message:"REJECTED!"
-        }
-        reject(errorObject);
+        // const errorObject={
+        //     message:"REJECTED!"
+        // }
+        reject(new Error('REJECTED!'));
     },300)
 });
 
