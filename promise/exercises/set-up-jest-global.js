b26c7c4a344b5ab99ac11dfa49d22bee3b19a691
swetha <swetha.gumpena0607@gmail.com>
2021-02-12 07:33:50 +0530

    feat(promises): Add global functions for multiple promises

--- promise/exercises/set-up-jest-global.js
+++ promise/exercises/set-up-jest-global.js
@@ -6,6 +6,17 @@ const second = (message) => {
   return Promise.resolve(message);
 };
 
+function getPromise1(){
+  return Promise.resolve('Promise1');
+}
+function getPromise2(){
+  return Promise.resolve('Promise2');
+}
+
+
 //for jest to know about these global
 global.first = first;
 global.second = second;
+
+global.getPromise1 = getPromise1
+global.getPromise2 = getPromise2
