3ff0710b9f25a302f5e914176cd77440aacdb88f
Anukriti <anukritigarg13@gmail.com>
2021-02-12 09:55:20 +0530

    fix(functional-js-workshop): Fix a bug in PartialApplyWithoutBind.js

--- javascript-home-work/functional-javascript-workshop/PartialApplyWithoutBind.js
+++ javascript-home-work/functional-javascript-workshop/PartialApplyWithoutBind.js
@@ -4,10 +4,12 @@ function logger(namespace) {
     return function logging(){
         var arrayArguments=slice.call(arguments);
         //arrayArguments.splice(0, 0, `${namespace.toUpperCase()}`);
-        [namespace].concat(arrayArguments); //namespace in [] to create it into an array
+        arrayArguments=[namespace].concat(arrayArguments); //namespace in [] to create it into an array
+        //console.log(arrayArguments);
         console.log.apply(null,arrayArguments);
         
     }
 }
-
+// const log=logger("Hello: ");
+// log("name","class");
 module.exports = logger
