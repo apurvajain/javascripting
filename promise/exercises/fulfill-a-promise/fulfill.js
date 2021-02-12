cdcaa536ac8553d56d78edf464426dead9127b0d
swetha <swetha.gumpena0607@gmail.com>
2021-02-12 07:39:30 +0530

    fix(promises): Update variable name for better read

--- promise/exercises/fulfill-a-promise/fulfill.js
+++ promise/exercises/fulfill-a-promise/fulfill.js
@@ -1,15 +1,18 @@
-var fulfillPromise = new Promise(function(resolve,reject) {
+var promise = new Promise(function(resolve,reject) {
     setTimeout(()=>{
         resolve("FULFILLED!")
     },300)
 })
 
 function usePromise(){
-    fulfillPromise.then((res)=>{
+    promise.then((res)=>{
         console.log(res)
 
     //above  two lines can be written as:
     //promise.then(console.log)
     })
 }
-module.exports = usePromise
+module.exports = {
+    promise,
+    usePromise
+}
