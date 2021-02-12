3032b03ef89c946842af53aa6233666a49468ae9
Anukriti <anukritigarg13@gmail.com>
2021-02-12 07:56:42 +0530

    fix(reject-promise): Test with new Error(REJECTEDrather that creating an object

--- promise/exercises/reject-promise/RejectPromise.test.js
+++ promise/exercises/reject-promise/RejectPromise.test.js
@@ -1,19 +1,14 @@
 const {promise,onReject}=require("./RejectPromise");
 
 test("Should reject the promise with ERROR object",(done)=>{
-    const errorObject={
-        message:"REJECTED!"
-    }
-    expect(promise).rejects.toEqual(errorObject);
+    expect(promise).rejects.toEqual(new Error('REJECTED!'));
     done()
 
 })
 
 test("Should reject value (REJECTED!) should be printed to console", (done) => {
     const consoleSpy = jest.spyOn(console, "log");
-    promise.then(()=>{
-
-    },onReject)
+    promise.then(()=>{},onReject)
     setTimeout(() => {
         expect(consoleSpy).toHaveBeenCalledWith("REJECTED!");
         done();
