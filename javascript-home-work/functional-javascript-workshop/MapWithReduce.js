7cc8411f36a10b6f19a1cda4728efba80217ef56
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-13 17:03:13 +0530

    feat(functional-js-workshop):add Map With Reduce exercise

--- javascript-home-work/functional-javascript-workshop/MapWithReduce.js
+++ javascript-home-work/functional-javascript-workshop/MapWithReduce.js
@@ -1,6 +1,6 @@
 module.exports = function arrayMap(arr, fn) {
   return arr.reduce((accumulator, currentValue, index, arr) => {
-    accumulator.push(fn(currentValue, index, arr));
+    accumulator.push(fn(currentValue));
     return accumulator;
   }, []);
 };
