0fb159d48d4e7bf447e845cd3d74cfbc7d326e85
shreya <shrreyarudresh@gmail.com>
2021-02-13 11:06:55 +0530

    feat: should return 2 when passed 2

--- tdd/final/fizzbuzz.test.js
+++ tdd/final/fizzbuzz.test.js
@@ -1,14 +1,16 @@
 const fizzbuz = require('./fizzbuzz')
 
 
+function checkFizzbuzz(value, exp) {
+    const result = fizzbuz(value);
+    expect(result).toBe(exp);
+}
 
-test('should return "1" when passed 1',()=>{
-    const result=fizzbuz(1);
-    expect(result).toBe("1");
+test('should return "1" when passed 1', () => {
+    checkFizzbuzz(1, "1");
 })
 
-test('should return "2" when passed 2',()=>{
-    const result=fizzbuz(2);
-    expect(result).toBe("2");
+test('should return "2" when passed 2', () => {
+    checkFizzbuzz(2,"2");
 })
 
