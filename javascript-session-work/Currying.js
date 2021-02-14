e3d47176a5fa6c3891dc15a240adda9ce2f0de99
asmita <asmitahajra@gmail.com>
2021-02-14 19:33:58 +0530

    feat(practice): update file currying example

--- javascript-session-work/Currying.js
+++ javascript-session-work/Currying.js
@@ -1,3 +1,20 @@
+//first we call function with x num of variables and that will return function.
+//then we call that func with y num of variables
+var babyAnimals = function(animal1) {
+    return function(animal2) {
+        return function(animal3) {
+        return `I love ${animal1}, ${animal2} and ${animal3}`;
+        };
+    };
+}
+
+//babyanimal takes a func with one arg, and returns a func w another arg
+
+//var babyKoala = babyAnimals('Koala');
+//console.log(babyKoala('Monkey'))
+//gives the ability to not replicate logic of calling koala
+console.log(babyAnimals('cats')('dogs')('ducks'))
+
 var calculatePercentage = function(cent) {
     return function(percentValue) {
         return function(percentOf){
