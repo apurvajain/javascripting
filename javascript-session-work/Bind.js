5963dc3b911659a28740465e4ec5baca47e4f54f
asmita <asmitahajra@gmail.com>
2021-02-14 19:18:10 +0530

    feat(practice): add file bind examples

---
+++ javascript-session-work/Bind.js
@@ -0,0 +1,37 @@
+// var pokemon = {
+//     firstname: 'Pika',
+//     lastname: 'Chu ',
+//     getPokeName: function() {
+//         var fullname = this.firstname + ' ' + this.lastname;
+//         return fullname;
+//     }
+// };
+
+// var pokemonName = function() {
+//     console.log(this.getPokeName() + 'I choose you!');
+// };
+
+// //console.log(pokemonName()); error cos no such func in global this
+
+// var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
+
+// logPokemon(); // 'Pika Chu I choose you!'
+
+var pokemon = {
+    firstname: 'Pika',
+    lastname: 'Chu ',
+    getPokeName: function() {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
+};
+
+var pokemonName = function(snack, hobby) {
+    console.log(this.getPokeName() + 'I choose you!');
+    console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
+};
+
+var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
+//returns a func which has provided context of this object. so to call just provide added args
+
+logPokemon('sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
