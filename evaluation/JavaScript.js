564b69f9a322fe2b98e7c2f0962eb9a9248c8128
asmita <asmitahajra@gmail.com>
2021-02-15 11:44:13 +0530

    feat(wealthprog): add file wealth management

--- evaluation/JavaScript.js
+++ evaluation/JavaScript.js
@@ -1,8 +1,153 @@
 const fetch = require("node-fetch");
+const { exitCode } = require("process");
 var names=[];
+var obj=[];
 function start()
 {
-    console.log("Hello")
+    console.log("Welcome to the user wealth management system")
+}
+
+function printList()
+{
+    for(object in obj)
+    {
+        console.log("name: "+obj[object].name+" wealth :"+obj[object].wealth);
+
+    }
+    console.log("---------------------------------");
+}
+
+function updateList()
+{
+    const prompt = require('prompt-sync')();
+    const newName=(prompt("Enter name "));
+    const newWealth=parseInt(prompt("Enter wealth "));
+
+    var newMember={name: newName, wealth: newWealth}
+    obj.push(newMember);
+    printList();
+    menu();
+
+}
+
+function exitCode1()
+{
+    throw new Error("Stop script");
+
+
+}
+
+function doubleMoney()
+{
+    for(object in obj)
+    {
+        obj[object].wealth=obj[object].wealth*2;
+        console.log("name: "+obj[object].name+" wealth :"+obj[object].wealth);
+
+    }
+    console.log("---------------------------------");
+
+    printList();
+    menu();
+
+}
+
+function calcTotalWealth()
+{
+    var sum=0;
+    for(object in obj)
+    {
+        sum+=obj[object].wealth;
+        //console.log("Total wealth is :"+sum);
+
+    }
+    console.log("Total wealth is :"+sum);
+    console.log("---------------------------------");
+
+    //printList();
+    menu();
+
+}
+
+function showMillionaire()
+{
+    for(object in obj)
+    {
+        if(obj[object].wealth>=1000000)
+        console.log("name: "+obj[object].name+" wealth :"+obj[object].wealth);
+
+    }
+    //printList();
+    menu();
+
+}
+
+function sortByRichest()
+{
+
+    //console.log(obj.sort((a, b) => (a.wealth < b.wealth) ? 1 : -1));
+    sorted=obj.sort((a, b) => (a.wealth < b.wealth) ? 1 : -1);
+    for(object in sorted)
+    {
+        console.log("name: "+sorted[object].name+" wealth :"+sorted[object].wealth);
+
+    }
+    menu();
+
+}
+
+function menu()
+{
+    console.log("Please select option")
+    console.log("1. Add user print new user list")
+    console.log("2. Double money and print updated list")
+    console.log("3. Show only millionaires")
+
+    console.log("4. Sort by richest")
+
+    console.log("5. Calculate total wealth and print")
+
+    console.log("6. Exit")
+
+    const prompt = require('prompt-sync')();
+    const option=parseInt(prompt("Enter option "));
+    //const var2=parseInt(prompt("Enter number 2"));
+
+    //const var3=var1+var2;
+    console.log("You have selected option"+option);
+    if(option==1)
+    {
+        updateList();
+    }
+    if(option==2)
+    {
+        doubleMoney();
+    }
+    if(option==3)
+    {
+        showMillionaire();
+    }
+    if(option==4)
+    {
+        sortByRichest();
+    }
+    if(option==5)
+    {
+        calcTotalWealth();
+    }
+    if(option==6)
+    {
+        //exitCode1();
+    }
+    //console.log("---------------------------------");
+
+
+
+}
+
+function selectOption()
+{
+
 }
 
 
@@ -52,7 +197,7 @@ function getdata()
         .then(function(data){
             var nameOfUser=getName(data);
             names.push(nameOfUser);
-            console.log(names)
+            //console.log(names)
             //console.log(typeof nameOfUser)
             //console.log(nameOfUser);
         });
@@ -77,22 +222,28 @@ function getdata()
 
 }
 
-function printNames()
-{
-    console.log(names)
-}
 
 function createList()
 {
-    var obj=[];
-    obj=[{name:names[0], wealth:}, {name:names[1], wealth:}, {name:names[2], wealth:}]
+    console.log(`<------- User Details: START-------->`);
+    obj=[{name:names[0], wealth:1200000}, {name:names[1], wealth:140000}, {name:names[2], wealth:160000}]
+    for(object in obj)
+    {
+        console.log("name: "+obj[object].name+"wealth :"+obj[object].wealth);
+
+    }
+    console.log(`<--------- User Details: END---------->`);
+
 
 }
 
 start();
 getdata();
-setTimeout(function(){ printNames(); }, 3000);
-createList();
+setTimeout(function(){ createList(); }, 2100);
+setTimeout(function(){ menu(); }, 2200);
+
+
+//createList();
 
 
 
