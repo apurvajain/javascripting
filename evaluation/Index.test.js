e496d769c4534587a8938099bb81004ce57b5625
Apoorva <apoorva.choudhary@iiitb.org>
2021-02-15 13:11:16 +0530

    feat(ev): add test fetch users should return an object

--- evaluation/Index.test.js
+++ evaluation/Index.test.js
@@ -1,4 +1,19 @@
 const {
-    promise,
-    usePromise 
-  } = require("./Promise");
+  fetchUsers,
+  addUser,
+  displayAllUsers,
+  setMoney,
+  clearLocalStorage,
+  fetchAllUsers,
+  doubleMoney,
+  showMillionares,
+  sortByRichest,
+  totalWealth
+  } = require("./Index");
+
+
+  test("fetch users should return an object", () => {
+    expect(typeof fetchUsers()).toBe('object');
+  });
+
+
