4d7906ed53920b83d80a63c7d853b40bd0458167
Anukriti <anukritigarg13@gmail.com>
2021-02-15 11:05:58 +0530

    feat(evaluation): Script is able to fetch new user and display the new user list

--- evaluation/EvalScript.sh
+++ evaluation/EvalScript.sh
@@ -3,3 +3,14 @@ echo "Welcome to the wealth Management Portal"
 
 node -e 'require("./javascript-files/initial-user-fetch/InitialUserFetch").fetchUsers(3)'
 node ./javascript-files/initial-user-log/InitialUserLog.js
+
+
+echo "\nPlease select option to continue \n1.Add new user and print new list"
+read option
+
+
+if [[ $option -eq 1 ]]
+then # if/then branch
+  node -e 'require("./javascript-files/initial-user-fetch/InitialUserFetch").fetchUsers(1)'
+  node ./javascript-files/initial-user-log/InitialUserLog.js
+fi
