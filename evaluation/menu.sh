9f95c2bea20a95f2b3d66367d0cd365477007838
anjali0508 <46600043+anjali0508@users.noreply.github.com>
2021-02-15 09:58:48 +0530

    feat(script): add basic script

---
+++ evaluation/menu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+option = 0
+while [[ "$option" != "6" ]]
+do
+    echo "Enter your option"
+    echo "1. Add user and print new user list"
+    echo "2. Double money of all users and print updated user list"
+    echo "3. Show only millionares"
+    echo "4. Sort by richest"
+    echo "5. Calculate total wealth of all users and print it"
+    echo "6. Exit"
+    read option
+    echo "You entered $option"
+done
