7f6c922ac04dcac647555927b301245ebd62423c
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-02-15 11:21:56 +0530

    test: Add functionality of double the money

--- evaluation/CallAPI.js
+++ evaluation/CallAPI.js
@@ -1,34 +1,43 @@
-module.exports = { fetch, fetchRandomUsers }
+const fetch = require("node-fetch");
+var LocalStorage = require("node-localstorage").LocalStorage,
+  localStorage = new LocalStorage("./scratch");
 
-var fetch = require("node-fetch");
-
-var users = []
-
-if (typeof localStorage === "undefined" || localStorage === null) {
-    var LocalStorage = require('node-localstorage').LocalStorage;
-    localStorage = new LocalStorage('./scratch');
-} else {
-    users = localStorage.getItem('users')
+var users = [];
+async function fetchUsers() {
+  try {
+    fetch("https://randomuser.me/api/?results=3")
+      .then((response) => response.json())
+      .then(function (data) {
+        data.results.forEach((item) => {
+          var obj = {};
+          obj["name"] =
+            item.name.title + " " + item.name.first + " " + item.name.last;
+          obj["wealth"] = Math.floor(
+            Math.random() * 10000 + Math.random() * 10000
+          );
+          users.push(obj);
+          localStorage.setItem("users", JSON.stringify(users));
+        });
+        console.log("<---User details: START--->");
+        console.log(users);
+        console.log("<---User details: END--->");
+      });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function fetchRandomUsers(numUsersToFetch) {
-for(i=0; i<numUsersToFetch; i++){
-fetch('https://randomuser.me/api')
-.then((resp) => {
-    return resp.json()
-})
-.then(function(jsonResponse) {
-    let responseWithWealth = jsonResponse['results'][0]
-    responseWithWealth['wealth'] = Math.random() * 100000   
-    console.log("Name: " + responseWithWealth.name.first + responseWithWealth.name.last + " wealth: " + responseWithWealth.wealth)
-    users.push(responseWithWealth)
-    localStorage.setItem('users', users);
- })
-}
+function doubleWealth() {
+  users.forEach((item) => {
+    return (item.wealth = item.wealth * 2);
+  });
+  console.log(users);
 }
 
+const millionaires = users.filter(function (users) {
+  return users.wealth > 100000;
+});
 
+fetchUsers();
 
-fetchRandomUsers(3)
-
-module.exports = {fetch, fetchRandomUsers}
+module.exports = { fetchUsers, doubleWealth };
--- evaluation/EvalScript.sh
+++ evaluation/EvalScript.sh
@@ -1,11 +1,24 @@
 echo "Welcome to the user wealth management system portal: "
 echo "<-----------------User details: start--------------------->"
-node -e 'require("./CallAPI").fetchRandomUsers()'
+node -e 'require("./CallAPI").fetchUsers(3)'
 echo "<-----------------User details: end--------------------->"
-echo "select an option from the given menu
-1. Add user and print new user list
-2. Double money of all users and print updated user list
-3. Show only millionares
-4. Sort by richest
-5. Calculate total wealth of all users and print it
-6. Exit"
+option = 0
+while [[ "$option" != "6" ]]
+do
+    echo "Enter your option"
+    echo "1. Add users and print new user list"
+    echo "2. Double the money of all users and print updated user list"
+    echo "3. Show only millionares"
+    echo "4. Sort by richest"
+    echo "5. Calculate total wealth of all users and print it"
+    echo "6. Exit"
+    read option
+    echo "You entered $option"
+    if [ "$option" == "1" ]
+    then node -e 'require("./CallAPI").fetchUsers(1)'
+    fi
+    if [ "$option" == "2" ]
+    then node -e 'require("./CallAPI.js").doubleWealth()'
+    fi
+
+done
