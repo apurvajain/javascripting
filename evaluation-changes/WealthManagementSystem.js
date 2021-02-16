f0689de5e2991146ecc4738537c19bc323b4d4a6
asmita <asmitahajra@gmail.com>
2021-02-16 23:46:35 +0530

    feat(evaluation-changes): add code and test for clearing user details

--- evaluation-changes/WealthManagementSystem.js
+++ evaluation-changes/WealthManagementSystem.js
@@ -1,8 +1,33 @@
+const {LocalStorage} = require("node-localstorage");
+var localStorage = new LocalStorage('./scratch'); 
 const fetch = require("node-fetch");
+
+function clearUserDetails() {
+    let allUsers = getAllUserDetails()
+    while (allUsers.length) {
+        allUsers.pop();
+    }
+    localStorage.setItem("allUsers", JSON.stringify(allUsers));
+  }
 
 function randomNumber(min, max) {  
     return Math.floor(Math.random() * (max - min) + min); 
 }  
+
+function getAllUserDetails() {
+    let users = JSON.parse(localStorage.getItem("allUsers") || "[]");
+    return users;
+}
+
+function storeUserDetails(allUserDetails) {
+   let allUsers=getAllUserDetails();
+   if(allUsers.length>0) {
+        allUsers.forEach((user)=> {
+            allUserDetails.push(user)
+        })
+   }
+   localStorage.setItem("allUsers", JSON.stringify(allUserDetails));
+}
 
 function fetchUser(url) {
     let randomWealth = randomNumber(80000, 2000000);
@@ -17,7 +42,8 @@ function fetchUser(url) {
             eachUserDetail.fullname=fullName;
             eachUserDetail.wealth=randomWealth;
             userDetails.push(eachUserDetail);
-            //storeUserDetails(userDetails);
+            //console.log(userDetails);
+            storeUserDetails(userDetails);
             return eachUserDetail;
         })
         .catch((error)=> {
@@ -26,5 +52,8 @@ function fetchUser(url) {
         })
 }
 
+
+
+
 //fetchUser("https://randomuser.me/api");
-module.exports={fetchUser}
+module.exports={fetchUser, clearUserDetails}
--- evaluation-changes/WealthManagementSystem.test.js
+++ evaluation-changes/WealthManagementSystem.test.js
@@ -1,6 +1,11 @@
-const{fetchUser}= require("./WealthManagementSystem");
+const{fetchUser, clearUserDetails}= require("./WealthManagementSystem");
 
 test('can call fetchUser', () => {
     fetchUser("url");
 })
 //to edit test for fetch call
+
+test('can call clearUserDetails', () => {
+    clearUserDetails();
+})
+////to edit test for clear
