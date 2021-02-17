bc795b2b656153ce7974efbad4bf3d048f385bd3
Anukriti <anukritigarg13@gmail.com>
2021-02-17 11:23:26 +0530

    refactor(Wealth Management System): Refactor test cases

--- evaluation/Users/Users.test.js
+++ evaluation/Users/Users.test.js
@@ -4,7 +4,7 @@ test('Local Storgae saves and retrives data', () => {
   const users = [{ name: 'Abhi', age: 21 }, { name: 'Adi', age: 22 }];
   user.saveUsers(users);
   const gotUsers = user.getUsers();
-  expect(gotUsers).toEqual([{ name: 'Abhi', age: 21 }, { name: 'Adi', age: 22 }]);
+  expect(gotUsers).toEqual(users);
 });
 
 test('Clear local storage and handle empty local storgae', () => {
