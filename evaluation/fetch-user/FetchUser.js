f64872a012852f304eb27c607506697cd76e246e
isha2912 <ishadeep222@gmail.com>
2021-02-17 23:24:02 +0530

    feat(test2) :  change functionalities

--- evaluation/fetch-user/FetchUser.js
+++ evaluation/fetch-user/FetchUser.js
@@ -3,15 +3,14 @@ const { LocalStorage } = require('node-localstorage');
 
 localStorage = new LocalStorage('./scratch');
 
-function getDetails() {
+
   let users = JSON.parse(localStorage.getItem('users'));
   if (!users) {
     users = [];
-  }
-  return users;
+
 }
 function fetchUser(userCount) {
-  const users = getDetails();
+  // const users = getDetails();
   for (let initialCount = 0; initialCount < userCount; initialCount += 1) {
     fetch('https://randomuser.me/api')
       .then((response) => response.json())
@@ -32,7 +31,7 @@ function fetchUser(userCount) {
 }
 
 function addAndDisplay() {
-  const userList = getDetails();
+  const userList = users;
   fetchUser(1);
   userList.forEach((user) => {
     console.log(`Name: ${user.name}  ` + `Wealth: ${user.wealth}`);
@@ -41,7 +40,7 @@ function addAndDisplay() {
 }
 
 function doubleWealth() {
-  const userList = getDetails();
+   const userList = users
   const doubledMoney = userList.map((user) => ({
     name: user.name,
     wealth: user.wealth * 2,
@@ -50,7 +49,7 @@ function doubleWealth() {
 }
 
 function showMillionaires() {
-  const userList = getDetails();
+   const userList = users
   userList.forEach((user) => {
     if (user.wealth > 10000000) {
       console.log(`Name: ${user.name}  ` + `Wealth: ${user.wealth}`);
@@ -59,7 +58,7 @@ function showMillionaires() {
 }
 
 function sortByRichest() {
-  const userList = getDetails();
+  const userList =users
   userList.sort((a, b) => b.wealth - a.wealth);
   localStorage.setItem('users', JSON.stringify(userList));
   userList.forEach((user) => {
@@ -67,8 +66,8 @@ function sortByRichest() {
   });
 }
 
-function totalWealth() {
-  const usersList = getDetails();
+ function totalWealth() {
+  const usersList = users;
   const totalWealth = usersList.reduce((accumulator, currentValue) => accumulator + currentValue.wealth, 0);
   console.log(totalWealth);
 }
