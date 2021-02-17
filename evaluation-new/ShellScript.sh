fc873718ad9f66b5759be94da96d01fa1e14172e
Priya Gupta <pgupta000205@gmail.com>
2021-02-17 22:34:29 +0530

    feat: Menu of different functionalities

---
+++ evaluation-new/ShellScript.sh
@@ -0,0 +1,51 @@
+start="<--------USER Details: START----------->"
+end="<--------USER Details: END--------------->"
+echo "Welcome to User Wealth Mangement System "
+echo $start
+node -e 'require("./FetchData").fetchNewUsers(3)'
+echo $end
+while :
+do
+
+    cat<<EOF
+
+Please select an option
+
+    Option (1) Add user and print new user list
+    Option (2) Double money of all users and print updated user list
+    Option (3) Show only millionares
+    Option (4) Sort by richest
+    Option (5) Calculate total wealth of all users and print it
+    Option (6) Exit
+           
+EOF
+    read -n1 -s
+    case "$REPLY" in
+    "1")  echo "You have selected option 1"
+    echo $start
+    
+    node -e 'require("./FetchData").fetchNewUsers(1)' 
+    echo $end ;;
+    "2")  echo "You have selected option 2"
+    echo $start
+    node -e 'require("./Functions").doubleWealth()'
+    echo $end ;;
+    "3")  echo "You have selected option 3"
+    echo $start
+    node -e 'require("./Functions").showMillionares()' 
+    echo $end ;;
+    "4")  echo "You have selected option 4"
+    echo $start
+    node -e 'require("./Functions").sortByWealth()' 
+    echo $end ;;
+    "5")  echo "You have selected option 5"
+    echo $start
+    node -e 'require("./Functions").calculateWealth()'
+    echo $end ;;
+    "6")  echo "You have selected option 6"
+    echo "Exit"
+    exit                      ;;
+     * )  echo "invalid option"     ;;
+    esac
+    sleep 1
+done
