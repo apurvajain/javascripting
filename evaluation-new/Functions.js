b9ad6f3f409262080959f3bc75d206167722ec6f
Priya Gupta <pgupta000205@gmail.com>
2021-02-17 22:32:55 +0530

    feat: Different functionalities

---
+++ evaluation-new/Functions.js
@@ -0,0 +1,41 @@
+var LocalStorage = require("node-localstorage").LocalStorage,
+    localStorage = new LocalStorage("./scratch1");
+
+function doubleWealth() {
+    const users = JSON.parse(localStorage.getItem("users"));
+    // console.log(typeof users);
+        for (obj in users) {
+            users[obj]["wealth"] *= 2;
+            console.log("FullName: " + users[obj]["fullName"] + "\tWealth: " + users[obj]["wealth"]);
+        }
+}
+
+function showMillionares() {
+    const users = JSON.parse(localStorage.getItem("users"));
+        for (obj in users) {
+            if (users[obj]["wealth"] > 1000000)
+                console.log("FullName: " + users[obj]["fullName"] + "\tWealth: " + users[obj]["wealth"]);
+        }
+}
+
+function sortByWealth() {
+    const users = JSON.parse(localStorage.getItem("users"));
+        const sorted = users.sort((a, b) => a["wealth"] < b["wealth"] ? 1 : -1)
+        for (obj in users) {
+            console.log("FullName: " + users[obj]["fullName"] + "\tWealth: " + users[obj]["wealth"]);
+        }
+}
+function calculateWealth() {
+    let sum = 0;
+    const users = JSON.parse(localStorage.getItem("users"));
+        for (obj in users)
+            sum += users[obj]["wealth"];
+    console.log(`Total sum: ${sum}`);
+}
+
+module.exports = {
+    doubleWealth,
+    showMillionares,
+    sortByWealth,
+    calculateWealth
+}
